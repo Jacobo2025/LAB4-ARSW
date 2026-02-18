0623b520f531098b35df04b9c4ed40b2c51478c0
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2026-02-18 16:45:36 -0500

    Feat: punto 2

--- src/main/java/edu/eci/arsw/blueprints/persistence/BlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/BlueprintPersistence.java
@@ -1,8 +1,10 @@
 package edu.eci.arsw.blueprints.persistence;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
-import java.util.Set;
+import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+@Repository
 public interface BlueprintPersistence {
 
     void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException;
