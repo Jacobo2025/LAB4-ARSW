165f6a8493cc7dfa2eb8f25baf09cb2013e1c74b
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2026-02-19 23:16:40 -0500

    feat: implement filters for Blueprints with unit tests

--- src/main/java/edu/eci/arsw/blueprints/model/Point.java
+++ src/main/java/edu/eci/arsw/blueprints/model/Point.java
@@ -1,10 +1,11 @@
 package edu.eci.arsw.blueprints.model;
 
 import jakarta.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
-public class Point{
-    private  int x;
+public class Point {
+    private int x;
     private int y;
 
     public Point(int x, int y) {
@@ -12,12 +13,20 @@ public class Point{
         this.y = y;
     }
 
-    public int getX() {
-        return x;
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public Point() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point p)) return false;
+        return x == p.x && y == p.y;
     }
 
-    public int getY() {
-        return y;
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
-    public Point(){}
 }
